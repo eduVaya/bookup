6bda1ebc0f4df02ca2f9d92724632602eae37e3e
eduVaya <evalladarespacheco@protonmail.com>
2025-09-19 18:44:09 -0400

    create .net backend

--- backend/routes/authenticationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { signup } from '../controllers/authenticationContrillers.js'
-
-const router = express.Router();
-
-router.post('/signup', signup);
-
-export default router;
